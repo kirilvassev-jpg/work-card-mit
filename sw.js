f46426370771f04d7a0dfc139f628ec6a3eb226e
kirilvassev-jpg <kiril.vassev@gmail.com>
2026-09-11 06:37:14 +0000

    Add PWA support (manifest, service worker, icons)

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'work-card-v1';
+const urlsToCache = [
+  '/work-card-mit/',
+  '/work-card-mit/index.html',
+  '/work-card-mit/history.html',
+  '/work-card-mit/manifest.json',
+  '/work-card-mit/icons/icon-192.png',
+  '/work-card-mit/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
